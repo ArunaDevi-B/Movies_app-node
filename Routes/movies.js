590afff377dd22850c126bfb3ca1fc59315e56e1
ArunaDevi-B <b.arunadevi30@gmail.com>
2022-09-12 10:44:11 +0530

    edited

--- Routes/movies.js
+++ Routes/movies.js
@@ -3,39 +3,9 @@ import { getMoviesById, deleteMovieById, getAllMovies, addMovie } from "../helpe
 
 const router = express.Router();
 
-// router.get("/movies",(req,res)=>{
-//     const { language } = req.query;
-//     console.log(req.query,language);
-//     let filteredMovies = movies;
-//     if(language) {
-//       filteredMovies = filteredMovies.filter((mv)=> mv.language === language);
-//     }
-
-//     res.send(filteredMovies);
-//   });
-//     const languages = movies.find((lng)=> {
-//       if(lng.language == language){
-//         return lng;
-//       }
-// });
-// res.send(movies.filter((mv)=> mv.language === language));
-// router.get("/movies", async (req, res)=>{
-//   const { mvs } = req.params;
-//   const movie = await client.db("b37wd").collection("movies").find({movie : mvs});
-//   console.log(movie);
-//     res.send(movie);
-// });
 
 router.get("/", async (req,res)=>{
-  // const { language,rating } = req.query;
-  // console.log(req.query.rating);
-  // let filteredRating = movies;
-  // if(language) {
-  //   filteredRating = filteredRating.filter((mv)=> mv.language === language);
-  //      }
-  // if(rating){
-  //   filteredRating = filteredRating.filter((mv)=> mv.rating === rating);
-  // }
+  
 console.log('check type: ', typeof req.query.rating)
   //getting data from mongodb
   console.log("req.query.rating", req.query.rating);
@@ -58,15 +28,7 @@ router.post("/", async (req,res)=>{
   res.send(movie);
 });
 
-// router.get("/movies",(req,res)=>{
-//     res.send(movies);
-// })
-// router.get("/movies", async (req, res)=>{
-//   const { mvs } = req.params;
-//   const movie = await client.db("b37wd").collection("movies").find({movie : mvs});
-//   console.log(movie);
-//     res.send(movie);
-// });
+
 
 
 //send only movie with the matched id
